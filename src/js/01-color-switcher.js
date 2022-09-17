11281f6e1e864902537036fc523f3c97fe79c38f
Serhii Lazarenok <serhiilazar90@gmail.com>
2022-09-18 00:44:25 +0300

    task 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const refs = {
+    bodyElement: document.querySelector('body'),
+    buttonStart: document.querySelector('[data-start]'),
+    buttonStop: document.querySelector('[data-stop]'),
+};
 
+refs.buttonStart.addEventListener('click', onClickStart);
+refs.buttonStop.addEventListener('click', onClickStop);
+
+
+let timerId = null;
+
+function onClickStart() {
+    refs.buttonStart.disabled = true;
+    refs.buttonStop.disabled = false;
+
+    timerId = setInterval(() => {
+        refs.bodyElement.style.background = getRandomHexColor()
+    }, 1000);
+};
+
+function onClickStop() {
+    refs.buttonStart.disabled = false;
+    refs.buttonStop.disabled = true;
+ 
+    clearInterval(timerId);
+
+};
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
